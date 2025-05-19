630657c8c5e1fa1cf900ae2639a67a11a647a083
Maximilian HUEBL <maximilian.huebl@ist.ac.at>
2025-05-19 17:34:25 +0200

    threading working

--- src/reverse_search.jl
+++ src/reverse_search.jl
@@ -103,8 +103,107 @@ restore!(neighcount::CachedNeighborCounter, args...) = pop!(neighcount.js)
 value(neighcount::SimpleNeighborCounter) = neighcount.j
 value(neighcount::CachedNeighborCounter) = last(neighcount.js)
 
-@enum RejectValue NOREJECT = 0 REJECTPOST = 1 REJECTPRE = 2 BREAKPOST = 3 BREAKPRE = 4
+@enum RejectValue NOREJECT = 0 REJECT = 1 BREAK = 2
 @enum RSStatus COMPLETE = 0 MAXVERTREACHED = 1 MAXDEPTHREACHED = 2 BREAKTRIGGERED = 3
+
+function rs(f, rsys::RSSystem, state::RSState; fargs=())
+    finished = false
+
+    while true
+        success = reverse_traverse!(state, rsys)
+        if success
+            reject_val = f(state.v, state.depth, fargs...)
+
+            if reject_val == BREAK
+                break
+            end
+            if reject_val == REJECT
+                forward_traverse!(state, rsys)
+                continue
+            end
+        else
+            success = forward_traverse!(state, rsys)
+            if !success 
+                finished = true
+                break
+            end
+        end
+    end
+    return finished
+end
+
+function _rsworker(f, rsys::RSSystem, input_queue, work_tokens, break_flag; depth_per_task, verts_per_task, fargs=())
+    hasf = !isnothing(f)
+
+    function callback(v, task_depth, start_depth, task_nv, args...)
+        # If another worker already broke, also break immedetely.
+        break_flag[] && return BREAK
+
+        total_depth = task_depth + start_depth
+
+        reject_val = hasf ? f(v, total_depth, args...) : NOREJECT
+
+        if reject_val == BREAK
+            Threads.atomic_or!(break_flag[], true)
+        elseif reject_val == NOREJECT && (task_nv[] >= verts_per_task || task_depth == depth_per_task)
+            reject_val = REJECT
+
+            if isinplace(rsys)
+                put!(input_queue, (copy(v), total_depth))
+            else
+                put!(input_queue, (v, total_depth))
+            end
+        end
+        return reject_val
+    end
+
+    while true
+        task_nv = Base.RefValue(1)
+
+        input = take!(input_queue)
+        isnothing(input) && break
+        v, start_depth = input
+
+        put!(work_tokens, true)
+
+        state = RSState(v; depth=0) # TODO pull out of this loop, then copy to it \\ add kwargs
+        rs(callback, rsys, state; fargs=(start_depth, task_nv, fargs...))
+
+        take!(work_tokens)
+
+        if break_flag[]
+            break
+        end
+    end
+    return
+end
+
+function prs(f, rsys::RSSystem, state::RSState; depth_per_task, verts_per_task, nthreads, fargs=())
+    input_queue = Channel{Union{Nothing,Tuple{typeof(state.v),Int}}}(Inf)
+
+    nworkers = min(Threads.nthreads(), nthreads) - 1
+    work_tokens = Channel{Bool}(nworkers)
+    break_flag = Threads.Atomic{Bool}(false)
+
+    put!(input_queue, (copy(state.v), state.depth))
+
+    tasks = [@spawn _rsworker(f, rsys, input_queue, work_tokens, break_flag; depth_per_task, verts_per_task, fargs) for _ in 1:nworkers]
+
+    while true
+        sleep(0.01)
+
+        if break_flag[] || (isempty(work_tokens) && isempty(input_queue))
+            # Terminate workers
+            for _ in 1:nworkers
+                put!(input_queue, nothing)
+            end
+            break
+        end
+    end
+
+    foreach(wait, tasks)
+    return
+end
 
 struct RSIterator{RSYS<:RSSystem,VTY}
     rssystem::RSYS
@@ -116,23 +215,16 @@ function RSIterator(ls, adj, v₀; compare=Base.:(==), cached=true, maxdepth=not
     rssystem = isnothing(isinplace) ? RSSystem(ls, adj, compare) : RSSystem{isinplace}(ls, adj, compare)
     return RSIterator(rssystem, v₀, cached, maxdepth)
 end
+function RSIterator(rsys::RSSystem, v₀; cached=true, maxdepth=nothing)
+    return RSIterator(rsys, v₀, cached, maxdepth)
+end 
 
 function Base.iterate(iter::RSIterator, state::RSState)
-    if state.depth == iter.maxdepth 
-        forward_traverse!(state, iter.rssystem)
-    end
-
-    success = reverse_traverse!(state, iter.rssystem)
-
-    if success
-        return state.v, state
+    finished = rs((_...)->ReverseSearch.BREAK, iter.rssystem, state)
+    if finished 
+        return nothing
     else
-        success = forward_traverse!(state, iter.rssystem)
-        if success
-            return Base.iterate(iter, state)
-        else
-            return nothing
-        end
+        return state.v, state
     end
 end
 
@@ -141,139 +233,222 @@ function Base.iterate(iter::RSIterator)
     return state.v, state
 end
 
-function reversesearch(rsys::RSSystem, state::RSState; maxdepth=Inf, maxverts=Inf, callback=nothing, callback_args=())
-    has_callback = !isnothing(callback)
 
-    lowest_depth = state.depth
-    break_triggered = false
-    nv = 1
+function reversesearch(f, rsys::RSSystem, state::RSState; threaded=false, kwargs...)
+    if threaded
+        return _reversesearch_multithread(f, rsys, state; kwargs...)
+    else
+        return _reversesearch_singlethread(f, rsys, state; kwargs...)
+    end
+end
 
-    while true
-        success = reverse_traverse!(state, rsys)
+function _reversesearch_singlethread(f, rsys::RSSystem, state::RSState; maxdepth=Inf, maxverts=Inf, fargs=())
+    hasf = !isnothing(f)
 
-        if success
-            reject_val = has_callback ? callback(state, callback_args...) : NOREJECT
+    maxdepth_flag = Base.RefValue(false)
+    maxvert_flag = Base.RefValue(false)
+    break_flag = Base.RefValue(false)
 
-            if reject_val == BREAKPRE
-                break_triggered = true
-                break
-            end
-            if reject_val == REJECTPRE
-                forward_traverse!(state, rsys)
-                continue
-            end
+    nv = Base.RefValue(1)
+    lowest_depth = Base.RefValue(1)
 
-            nv += 1
-            lowest_depth = max(state.depth, lowest_depth)
+    function callback(v, depth, args...)
+        at_maxdepth = depth == maxdepth
 
-            if reject_val == BREAKPOST || nv >= maxverts
-                break_triggered = true
-                break
-            end
-            if reject_val == REJECTPOST || state.depth >= maxdepth 
-                forward_traverse!(state, rsys)
-                continue
-            end
+        maxdepth_flag[] = maxdepth_flag[] || at_maxdepth
+        maxvert_flag[] = maxvert_flag[] || nv[] >= maxverts
+
+        if !maxvert_flag[]
+            reject_val = hasf ? f(v, depth, args...) : NOREJECT
         else
-            success = forward_traverse!(state, rsys)
-            !success && break
+            reject_val = BREAK
         end
+
+        if reject_val == NOREJECT
+            nv[] += 1
+            lowest_depth[] = max(lowest_depth[], depth)
+            if at_maxdepth
+                reject_val = REJECT
+            end
+        elseif reject_val == BREAK
+            break_flag[] = true
+        end
+
+        return reject_val
     end
 
-    if break_triggered
+    rs(callback, rsys, state; fargs)
+
+    if break_flag[]
         result = BREAKTRIGGERED
-    elseif nv == maxverts
-        result = MAXVERTREACHED # TODO: extra state for when both vertices and depth reached
-    elseif lowest_depth == maxdepth
+    elseif maxvert_flag[]
+        result = MAXVERTREACHED
+    elseif maxdepth_flag[]
         result = MAXDEPTHREACHED
     else
         result = COMPLETE
     end
 
-    return (; result, nv, lowest_depth)
+    return result, nv[], lowest_depth[]
 end
 
-function rs_worker(rsys::RSSystem, input_queue, work_tokens, stop_signal, total_nv, lowest_depth; depth_per_task, verts_per_task, maxdepth, maxverts, callback=nothing, callback_args=())
-    has_callback = !isnothing(callback)
+# TODO: this is the exact same code, except that Base.RefValue is replace with atomics
+# Maybe there is a simple macro that can eliminate the need for duplicate code
+function _reversesearch_multithread(f, rsys::RSSystem, state::RSState; depth_per_task, verts_per_task, nthreads=Threads.nthreads(), maxdepth=Inf, maxverts=Inf, fargs=())
+    hasf = !isnothing(f)
 
-    function worker_callback(state, task_nv, start_depth, args...)
-        stop_signal[] && return BREAKPRE
-        total_nv[] >= maxverts && return BREAKPRE
+    maxdepth_flag = Threads.Atomic{Bool}(false)
+    maxvert_flag = Threads.Atomic{Bool}(false)
+    break_flag = Threads.Atomic{Bool}(false)
 
-        reject_val = has_callback ? callback(state, args...) : NOREJECT
+    nv = Threads.Atomic{Int}(1)
+    lowest_depth = Threads.Atomic{Int}(1)
 
-        if reject_val == NOREJECT || reject_val == REJECTPOST || reject_val == BREAKPOST
-            task_nv[] += 1
+    function callback(v, depth, args...)
+        at_maxdepth = depth == maxdepth
+
+        Threads.atomic_or!(maxdepth_flag, at_maxdepth)
+        Threads.atomic_or!(maxvert_flag, nv[] >= maxverts)
+
+        if !maxvert_flag[]
+            reject_val = hasf ? f(v, depth, args...) : NOREJECT
+        else
+            reject_val = BREAK
         end
-        
-        # If max number of vertices have been visited,
-        # stop going to the children of new vertices
-        # and add the vertex to the input queue
-        total_depth = state.depth + start_depth
-        if (task_nv[] >= verts_per_task || state.depth == depth_per_task) && reject_val == NOREJECT && total_depth < maxdepth
-            reject_val = REJECTPRE
-            if isinplace(rsys)
-                put!(input_queue, (copy(state.v), total_depth))
-            else
-                put!(input_queue, (state.v, total_depth))
+
+        if reject_val == NOREJECT
+            Threads.atomic_add!(nv, 1)
+            Threads.atomic_max!(lowest_depth, depth)
+            if at_maxdepth
+                reject_val = REJECT
             end
+        elseif reject_val == BREAK
+            Threads.atomic_or!(break_flag, true)
         end
+
         return reject_val
     end
 
-    # worker_maxdepth = 0
+    prs(callback, rsys, state; depth_per_task, verts_per_task, nthreads, fargs)
 
-    while true
-        task_nv = Base.RefValue(1)
-
-        input = take!(input_queue)
-        isnothing(input) && break
-
-        v, start_depth = input
-
-        put!(work_tokens, true)
-
-        state = RSState(v; depth=0) # TODO make general
-        result, nv, depth_reached = reversesearch(rsys, state; maxdepth=maxdepth-start_depth, callback=worker_callback, callback_args=(task_nv, start_depth, callback_args...))
-
-        Threads.atomic_add!(total_nv, nv)
-        Threads.atomic_max!(lowest_depth, start_depth + depth_reached)
-        # worker_maxdepth = max(worker_maxdepth, start_depth + depth_reached)
-
-        take!(work_tokens)
-
-        if result == BREAKTRIGGERED || result == MAXVERTREACHED
-            stop_signal[] = true
-            break
-        end
+    if break_flag[]
+        result = BREAKTRIGGERED
+    elseif maxvert_flag[]
+        result = MAXVERTREACHED
+    elseif maxdepth_flag[]
+        result = MAXDEPTHREACHED
+    else
+        result = COMPLETE
     end
-    return
+
+    return result, nv[], lowest_depth[]
 end
 
-function rs_parallel(rsys::RSSystem, state::RSState; depth_per_task, verts_per_task, maxdepth=Inf, maxverts=Inf, callback=nothing, callback_args=())
-    input_queue = Channel{Union{Nothing,Tuple{typeof(state.v),Int}}}(Inf)
 
-    nworkers = nthreads() - 1
-    work_tokens = Channel{Bool}(nworkers)
-    stop_signal = Threads.Atomic{Bool}(false)
-    nv = Threads.Atomic{Int}(0)
-    lowest_depth = Threads.Atomic{Int}(0)
 
-    put!(input_queue, (copy(state.v), state.depth))
 
-    tasks = [@spawn rs_worker(rsys, input_queue, work_tokens, stop_signal, nv, lowest_depth; depth_per_task, verts_per_task, maxdepth, maxverts, callback, callback_args) for _ in 1:nworkers]
 
-    while true
-        sleep(0.01)
 
-        if stop_signal[] || (isempty(work_tokens) && isempty(input_queue))
-            # Terminate workers
-            for _ in 1:nworkers
-                put!(input_queue, nothing)
-            end
-            break
-        end
-    end
-    foreach(wait, tasks)
-    return nv[], lowest_depth[]
-end
+# function rsworker(f, rsys::RSSystem, input_queue, work_tokens, total_nv, lowest_depth, flags; depth_per_task, verts_per_task, maxdepth, maxverts, fargs=())
+#     hasf = !isnothing(f)
+#     break_flag, maxdepth_flag, maxvert_flag = flags
+
+#     function callback(v, task_depth, task_nv, start_depth, args...)
+#         # If another worker already broke, also break immedetely.
+#         (break_flag[] || maxvert_flag[]) && return BREAK
+
+#         total_depth = task_depth + start_depth
+#         at_maxdepth = total_depth == maxdepth
+
+#         Threads.atomic_or!(maxdepth_flag[], at_maxdepth)
+#         Threads.atomic_or!(maxvert_flag[], total_nv[] >= maxverts)
+
+#         if !maxvert_flag[]
+#             reject_val = hasf ? f(v, depth, args...) : NOREJECT
+#         else
+#             reject_val = BREAK
+#         end
+
+#         if reject_val == NOREJECT
+#             Threads.atomic_add!(total_nv, 1)
+#             Threads.atomic_add!(task_nv, 1)
+#             Threads.atomic_max!(lowest_depth[], total_depth)
+#             if at_maxdepth
+#                 reject_val = REJECT
+#             end
+#         elseif reject_val == BREAK
+#             Threads.atomic_or!(break_flag[], true)
+#         end
+
+#         # If max number of vertices have been visited,
+#         # stop going to the children of new vertices
+#         # and add the vertex to the input queue
+#         if reject_val == NOREJECT && !at_maxdepth &&
+#             (task_nv[] >= verts_per_task || task_depth == depth_per_task)
+
+#             reject_val = REJECT
+
+#             if isinplace(rsys)
+#                 put!(input_queue, (copy(v), total_depth))
+#             else
+#                 put!(input_queue, (v, total_depth))
+#             end
+#         end
+#         return reject_val
+#     end
+
+#     while true
+#         task_nv = Base.RefValue(1)
+
+#         input = take!(input_queue)
+#         isnothing(input) && break
+
+#         v, start_depth = input
+
+#         put!(work_tokens, true)
+
+#         state = RSState(v; depth=0) # TODO pull out of this loop, then copy to it \\ add kwargs
+#         rs(callback, rsys, state; fargs=(task_nv, start_depth, fargs...))
+
+#         take!(work_tokens)
+
+#         if break_flag[] || maxvert_flag[]
+#             break
+#         end
+#     end
+#     return
+# end
+
+# function reversesearch_multithread(f, rsys::RSSystem, state::RSState; depth_per_task, verts_per_task, maxdepth=Inf, maxverts=Inf, fargs=())
+#     input_queue = Channel{Union{Nothing,Tuple{typeof(state.v),Int}}}(Inf)
+
+#     nworkers = nthreads() - 1
+#     work_tokens = Channel{Bool}(nworkers)
+
+#     nv = Threads.Atomic{Int}(0)
+#     lowest_depth = Threads.Atomic{Int}(0)
+
+#     break_flag = Threads.Atomic{Bool}(false)
+#     maxdepth_flag = Threads.Atomic{Bool}(false)
+#     maxvert_flag = Threads.Atomic{Bool}(false)
+
+#     flags = (break_flag, maxdepth_flag, maxvert_flag)
+
+#     put!(input_queue, (copy(state.v), state.depth))
+
+#     tasks = [@spawn rsworker(f, rsys, input_queue, work_tokens, nv, lowest_depth, flags; depth_per_task, verts_per_task, maxdepth, maxverts, fargs) for _ in 1:nworkers]
+
+#     while true
+#         sleep(0.01)
+
+#         if break_flag[] || (isempty(work_tokens) && isempty(input_queue))
+#             # Terminate workers
+#             for _ in 1:nworkers
+#                 put!(input_queue, nothing)
+#             end
+#             break
+#         end
+#     end
+#     foreach(wait, tasks)
+#     return nv[], lowest_depth[]
+# end
