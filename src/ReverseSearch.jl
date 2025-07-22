56830060050b282a9b900766e8cfe6f47f28032c
Maximilian HUEBL <maximilian.huebl@ist.ac.at>
2025-07-22 16:02:58 +0200

    stop exporting rs and prs

--- src/ReverseSearch.jl
+++ src/ReverseSearch.jl
@@ -3,7 +3,7 @@ module ReverseSearch
 import SciMLBase
 using Base.Threads: nthreads, @spawn
 
-export RSSystem, RSState, RSIterator, rs, prs, reversesearch
+export RSSystem, RSState, RSIterator, reversesearch
 
 include("./reverse_search.jl")
 include("./utils.jl")
