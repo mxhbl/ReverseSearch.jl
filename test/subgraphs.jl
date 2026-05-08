0043d0123f2de02c3c922be81aef366713ac484f
Maximilian HUEBL <maximilian.huebl@ist.ac.at>
2026-05-08 16:20:28 +0200

    add test for breaktriggered

--- test/subgraphs.jl
+++ test/subgraphs.jl
@@ -29,7 +29,7 @@ function subgraphsearch(G)
     return ls, adj
 end
 
-function testall(G, nv=nothing, depth=nothing; maxverts=Inf, maxdepth=Inf, testcollect=false, result=ReverseSearch.Finished, parallel_nv=nv)
+function testall(G, nv=nothing, depth=nothing; maxverts=Inf, maxdepth=Inf, testcollect=false, result=ReverseSearch.Finished, parallel_nv=nv, break_at_one=false)
     ls, adj = subgraphsearch(G)
 
     rsys = RSSystem(ls, adj, Int[])
@@ -56,6 +56,26 @@ function testall(G, nv=nothing, depth=nothing; maxverts=Inf, maxdepth=Inf, testc
     r_mt_nocache     = reversesearch(rsys; threaded=true, depth_per_task=10, verts_per_task=500, cache=CacheNothing(), maxdepth, maxverts)
     r_mt_countercache = reversesearch(rsys; threaded=true, depth_per_task=10, verts_per_task=500, cache=CacheCounter(), maxdepth, maxverts)
     r_st_aux         = reversesearch(rsys_aux; threaded=false, cache=CacheAll(), maxdepth, maxverts)
+
+    if break_at_one
+        count_st = Ref(0)
+        r_break_st = reversesearch(rsys; threaded=false, cache=CacheAll()) do v, depth
+            count_st[] += 1
+            count_st[] >= 1 ? BREAK : ACCEPT
+        end
+        @test r_break_st.status == BreakTriggered
+        @test r_break_st.nvertices == 1
+
+        count_mt = Threads.Atomic{Int}(0)
+        r_break_mt = reversesearch(rsys; threaded=true, depth_per_task=10, verts_per_task=500, cache=CacheAll()) do v, depth
+            Threads.atomic_add!(count_mt, 1)
+            count_mt[] >= 1 ? BREAK : ACCEPT
+        end
+
+        @test r_break_mt.status == BreakTriggered
+        @test r_break_mt.nvertices >= 1
+        return # return because breaking messed up all other return values
+    end
 
     if !isnothing(result)
         @test r_st_cache.status == result
@@ -111,6 +131,7 @@ end
 
     G = complete_graph(5)
     testall(G, 2 ^ 5, 5; testcollect=true)
+    testall(G, 2 ^ 5, 5; break_at_one=true)
 
     G = complete_graph(8)
     testall(G, 2 ^ 8, 8; testcollect=true)
